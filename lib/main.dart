eece894010ccb84827029d4d8c7e55905a2e24e5
pandazim <jaotoledo74@gmail.com>
2020-02-24 21:04:31 -0400

    Login Screen feita

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'MyHomePage.dart';
+
+void main() => runApp(MyApp());
+
+
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Login UI",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(title: 'Flutter Login'),
+    );
+  }
+}
